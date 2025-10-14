e2db4b587ecfd2e0fcbf82acafabe9af4a9c091a
Roman Shapiro <eleroman@mail.ru>
2025-10-14 11:13:24 +0700

    Fix tests

--- Tests/DigitalRiseModel.Tests/TangentFramesTests.cs
+++ Tests/DigitalRiseModel.Tests/TangentFramesTests.cs
@@ -7,34 +7,6 @@ namespace DigitalRiseModel.Tests
 	public sealed class TangentFramesTests
 	{
 		[TestMethod]
-		public void TestTangentFrames()
-		{
-			var manager = Utility.CreateAssetManager();
-
-			// Firstly load model as it is
-			var sourceModel = manager.LoadModel(TestsEnvironment.GraphicsDevice, "AlphaDeadTree.glb");
-
-			// Now load same model forcing tangents generation
-			// Since settings are part of cache key, those two models will be loaded/stored separately
-			var generatedModel = manager.LoadModel(TestsEnvironment.GraphicsDevice, "AlphaDeadTree.glb", TangentsGeneration.Always);
-
-			// Now compare original and generated tangents and binormals
-			for(var i = 0; i < sourceModel.Meshes.Length; ++i)
-			{
-				var sourceMesh = sourceModel.Meshes[i];
-				var generatedMesh = generatedModel.Meshes[i];
-
-				for(var j = 0; j < sourceMesh.MeshParts.Count; ++j)
-				{
-					var sourceData = sourceMesh.MeshParts[j].VertexBuffer.To2DArray();
-					var destData = generatedMesh.MeshParts[j].VertexBuffer.To2DArray();
-
-					Utility.CompareData(sourceData, destData);
-				}
-			}
-		}
-
-		[TestMethod]
 		public void TestTangentFramesGenerated()
 		{
 			var manager = Utility.CreateAssetManager();
@@ -50,8 +22,9 @@ namespace DigitalRiseModel.Tests
 				{
 					var part = mesh.MeshParts[j];
 
-					part.VertexBuffer.VertexDeclaration.EnsureElement(VertexElementUsage.Tangent);
-					part.VertexBuffer.VertexDeclaration.EnsureElement(VertexElementUsage.Binormal);
+					var ve = part.VertexBuffer.VertexDeclaration.EnsureElement(VertexElementUsage.Tangent);
+
+					Assert.AreEqual(VertexElementFormat.Vector4, ve.VertexElementFormat);
 				}
 			}
 		}
